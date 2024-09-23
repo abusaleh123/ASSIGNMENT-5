0dd1a77188d6a3eecaeb2ecd4da80a98f422e850
abusaleh123 <abusalehnoor4103@gmail.com>
2024-09-24 00:21:09 +0600

    added modal and addmoney section

--- add-money.js
+++ add-money.js
@@ -1,4 +1,61 @@
 document.getElementById('donate-btn').addEventListener('click', function(event){
     event.preventDefault();
-    console.log('clicked');
+    const addMoney = getInputFieldById('donate-money-input');
+    console.log(addMoney);
+    const accountBalance = getInputFieldByInnerText('accountBalance');
+
+    if(!isNaN(addMoney) && addMoney > 0){
+     const mainAccountBalance = getInputFieldByInnerText('main-account-balance');
+     console.log(mainAccountBalance);
+
+     const newBalance = mainAccountBalance + addMoney;
+     console.log(newBalance);
+     const mainNewBalance = accountBalance - addMoney;
+     document.getElementById('main-account-balance').innerText = newBalance;
+     document.getElementById('accountBalance').innerText = mainNewBalance;
+     my_modal_1.showModal();
+    }
+    else{
+        alert('Invalid Donation Amount')
+    }
+})
+
+
+document.getElementById('feni-donate-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoney = getInputFieldById('feni-donate-money-input');
+    console.log(addMoney);
+
+    if(!isNaN(addMoney) && addMoney > 0){
+     const mainAccountBalance = getInputFieldByInnerText('feni-main-account-balance');
+     console.log(mainAccountBalance);
+
+     const newBalance = mainAccountBalance + addMoney;
+     console.log(newBalance);
+     document.getElementById('feni-main-account-balance').innerText = newBalance;
+     my_modal_1.showModal();
+    }
+    else{
+        alert('Invalid Donation Amount')
+    }
+})
+
+document.getElementById('quota-donate-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    const addMoney = getInputFieldById('quota-donate-money-input');
+    console.log(addMoney);
+
+    if(!isNaN(addMoney) && addMoney > 0){
+     const mainAccountBalance = getInputFieldByInnerText('quota-main-account-balance');
+     console.log(mainAccountBalance);
+
+     const newBalance = mainAccountBalance + addMoney;
+     console.log(newBalance);
+     document.getElementById('quota-main-account-balance').innerText = newBalance;
+     my_modal_1.showModal();
+        
+    }
+    else{
+        alert('Invalid Donation Amount')
+    }
 })
--- shared.js
+++ shared.js
@@ -1,3 +1,19 @@
 function getInputFieldById (id){
     const addMoney = document.getElementById(id).value;
+
+    const inputNumber = parseFloat(addMoney);
+    return inputNumber;
 }
+
+function getInputFieldByInnerText (id){
+    const accountBAlance = document.getElementById(id).innerText;
+    const inputNumber = parseFloat(accountBAlance);
+    return(inputNumber);
+}
+
+
+// function showElementById(id){
+//     const showElement = document.getElementById('quota-donate-btn').classList.add('hidden');
+
+//     const show  = document.getElementById(id).classList.remove('hidden')
+// }
