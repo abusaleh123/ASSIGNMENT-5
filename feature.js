c330b429c9525ec5a747b33a393132a5ddecb611
abusaleh123 <abusalehnoor4103@gmail.com>
2024-09-24 11:46:21 +0600

    Donation and history button function done

--- feature.js
+++ feature.js
@@ -1,15 +1,15 @@
-// document.getElementById('history-btn').addEventListener('click', function(){
-//     window.location.href='./history.html'
-// });
-// document.getElementById('donation-btn').addEventListener('click', function(){
-//     window.location.href='./index.html'
-// });
-
 
 document.getElementById('history-btn').addEventListener('click', function(){
     showElementById('transaction-section');
 })
 document.getElementById('donation-btn').addEventListener('click', function(){
     showElementById('addMoney-container');
+})
+
+document.getElementById('history-btn').addEventListener('click', function(){
+    document.getElementById('donation-btn').classList.remove('bg-[#b4f461]');
+})
+document.getElementById('donation-btn').addEventListener('click', function(){
+    document.getElementById('donation-btn').classList.add('bg-[#b4f461]');
 })
 
--- history.js
+++ history.js
@@ -1,7 +1,7 @@
-const section = document.createElement("div");
-div.classList.add("bg-white");
-div.innerHTML = `
-<h4 class= "text-2xl font-bold text-black"> Cash Out</h4>
-<p>${addMoney} withdraw.New Balance: ${newBalance}</p>
-`;
-document.getElementById("transaction-container").appendChild(div);
+// const section = document.createElement("div");
+// div.classList.add("bg-white");
+// div.innerHTML = `
+// <h4 class= "text-2xl font-bold text-black"> Cash Out</h4>
+// <p>${addMoney} withdraw.New Balance: ${newBalance}</p>
+// `;
+// document.getElementById("transaction-container").appendChild(div);
